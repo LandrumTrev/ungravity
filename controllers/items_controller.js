6461dad1d0a684117307b65fa53ff257628bd9b4
LandrumTrev <landrum.trevillian@gmail.com>
2018-11-07 21:21:16 -0500

    fix script locations

--- controllers/items_controller.js
+++ controllers/items_controller.js
@@ -88,23 +88,23 @@ router.post("/api/items", function (req, res) {
 
 // ========================================================
 
+// PUT updates an item 
+router.put("/api/items/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
 
-// router.put("/api/items/:id", function(req, res) {
-//   var condition = "id = " + req.params.id;
+  console.log("condition", condition);
 
-//   console.log("condition", condition);
-
-//   item.update({
-//     sleepy: req.body.sleepy
-//   }, condition, function(result) {
-//     if (result.changedRows == 0) {
-//       // If no rows were changed, then the ID must not exist, so 404
-//       return res.status(404).end();
-//     } else {
-//       res.status(200).end();
-//     }
-//   });
-// });
+  item.update({
+    sleepy: req.body.sleepy
+  }, condition, function(result) {
+    if (result.changedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
 
 
 // ========================================================
