03d921fbc10b6078f8bf93c0b198c25f260aedc4
LandrumTrev <landrum.trevillian@gmail.com>
2018-11-10 19:20:40 -0500

    update server.js comments

--- server.js
+++ server.js
@@ -1,36 +1,46 @@
+// ====================================================
+// UnGravity :: A workout checklist web app
+// MVC with MySQL, Node, Express, Handlebars and custom ORM.
+// ©2018 Richard Trevillian
+// University of Richmond (Virginia)
+// Full Stack Developer Bootcamp (July 2018)
+// ====================================================
+// SERVER.JS - Node.js Express Server configuration
+// ====================================================
+
+// require the Express server module
 var express = require("express");
 
+// set port to run either on Heroku default or 8080 for localhost
 var PORT = process.env.PORT || 8080;
 
+// shorthand handle for running Express module, to access its method
 var app = express();
 
-// Serve static content for the app from the "public" directory in the application directory.
+// allow static files (css, js, html) to be served from the PUBLIC folder
 app.use(express.static("public"));
 
-// allow Express server to server static files from PUBLIC directory
-// is this needed? for external CSS files?
-// app.use(express.static('public'));
-// app.use('/css', express.static(__dirname + 'app/public/css'));
-// app.use(express.static(__dirname + 'app/public'));
-
-
-// Parse application body as JSON
+// allow Express to parse URLs, and include encoded Arrays and Objects
 app.use(express.urlencoded({ extended: true }));
+// allow Express to parse JSON body data
 app.use(express.json());
 
-// Set Handlebars.
+// require the Express-version of Handlebars HTML templating engine
 var exphbs = require("express-handlebars");
 
+// use express().engine method to set Handlebars as HTML template engine
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+// use express().set to Handlebars as the View Engine
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
+// import express.Router() method route definitions exported by items_controller.js
 var routes = require("./controllers/items_controller.js");
 
+// tell Express (app) to .use the (routes) exported from items_controller.js
 app.use(routes);
 
-// Start our server so that it can begin listening to client requests.
+// start Express server listening on the PORT(s) defined above
 app.listen(PORT, function() {
-  // Log (server-side) when our server has started
+  // log (server console) confirmation when server starts/restarts
   console.log("Server listening on: http://localhost:" + PORT);
 });
